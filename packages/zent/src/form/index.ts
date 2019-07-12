bbb10452ab410815c9a342496a0dafc74933dc0b
intellild <intellild@outlook.com>
2019-07-12 10:57:16 +0800

    fix form exports

--- packages/zent/src/form/index.ts
+++ packages/zent/src/form/index.ts
@@ -12,5 +12,7 @@ export * from './Control';
 export * from './Error';
 export * from './Label';
 export * from './FieldSet';
+export * from './Field';
+export * from './FieldSet';
 
 export { FieldArrayModel, FieldModel, FieldSetModel, FormStrategy, Validators };
