300194137eed38c00d5f90d97c1af7f0ba1ed0a6
intellild <intellild@outlook.com>
2019-07-17 15:59:36 +0800

    form date range picker

--- packages/zent/src/form/form-components/DateRangePickerField.tsx
+++ packages/zent/src/form/form-components/DateRangePickerField.tsx
@@ -1,38 +1,37 @@
 import * as React from 'react';
 import { Omit } from 'utility-types';
 import { DatePickers } from '../../datetimepicker/common/types';
-import {
-  useField,
-  noopMapEventToValue,
-  IFormComponentCommonProps,
-  IFormFieldModelProps,
-} from '../shared';
 import DateRangePicker, {
   IDateRangePickerProps,
 } from '../../datetimepicker/DateRangePicker';
 import { FormField } from '../Field';
+import { IFormComponentProps, IFormFieldChildProps } from '../shared';
 
-export interface IFormDateRangePickerFieldProps
-  extends IFormComponentCommonProps<
-    DatePickers.RangeValue,
-    Omit<IDateRangePickerProps, 'value'>
-  > {}
+export type IFormDateRangePickerFieldProps = IFormComponentProps<
+  DatePickers.RangeValue,
+  Omit<IDateRangePickerProps, 'value'>
+>;
 
 function dateDefaultValueFactory(): DatePickers.RangeValue {
   return [new Date(), new Date()];
 }
 
+function renderDateRangePicker(
+  childProps: IFormFieldChildProps<DatePickers.RangeValue>,
+  props: IFormDateRangePickerFieldProps
+) {
+  return <DateRangePicker {...props.props} {...childProps} />;
+}
+
 export const FormDateRangePickerField: React.FunctionComponent<
-  IFormDateRangePickerFieldProps & IFormFieldModelProps<DatePickers.RangeValue>
+  IFormDateRangePickerFieldProps
 > = props => {
-  const [childProps, { error }, ref] = useField<
-    DatePickers.RangeValue,
-    DatePickers.RangeValue,
-    IDateRangePickerProps
-  >(props, dateDefaultValueFactory, noopMapEventToValue);
   return (
-    <FormField ref={ref} {...props} error={error}>
-      <DateRangePicker {...props.props} {...childProps} />
+    <FormField
+      {...props}
+      defaultValue={props.defaultValue || dateDefaultValueFactory}
+    >
+      {childProps => renderDateRangePicker(childProps, props)}
     </FormField>
   );
 };
